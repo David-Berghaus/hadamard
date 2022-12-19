34f7901b049c9cf50c473c644fcf48f599238574
David Berghaus <s6ddberg@uni-bonn.de>
2022-12-19 12:14:07 +0100

    Simplify parameter modifications

--- PPO/foo.py
+++ PPO/foo.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import A2C, PPO, DQN, HER 
+from stable_baselines3 import A2C, PPO, DQN 
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.logger import configure
 import torch
@@ -8,12 +8,26 @@ from env import HadamardMlpEnv, HadamardMlpFlippingEnv
 tmp_path = "./tmp/sb3_log/"
 new_logger = configure(tmp_path, ["tensorboard"])
 
-#E = HadamardMlpEnv(20)
-E = HadamardMlpFlippingEnv(20)
-model = PPO('MlpPolicy', E, learning_rate=5e-5, verbose=1)
-#model = DQN('MlpPolicy', E, learning_rate=5e-5, verbose=1)
-model.set_logger(new_logger)
-model.learn(5000000)
-mean_reward, std_reward = evaluate_policy(model, E, n_eval_episodes=100)
+N = 20
+lr = 5e-5
+policy = "MlpPolicy"
+algorithm = "A2C"
+iteration_training_steps = 1000000
+# Custom actor (pi) and value function (vf) networks
+# of specified layer dimensionalities each with Relu activation function
+# Note: an extra linear layer will be added on top of the pi and the vf nets, respectively
+policy_kwargs = dict(activation_fn=torch.nn.ReLU,
+                     net_arch=[dict(pi=[128, 64, 4], vf=[128, 64, 4])])
 
-print(f"mean_reward:{mean_reward:.2f} +/- {std_reward}")
+E = HadamardMlpEnv(N)
+#E = HadamardMlpFlippingEnv(N)
+if algorithm == "PPO":
+    model = PPO(policy, E, learning_rate=lr, verbose=1, policy_kwargs=policy_kwargs)
+elif algorithm == "A2C":
+    model = A2C(policy, E, learning_rate=lr, verbose=1, policy_kwargs=policy_kwargs)
+elif algorithm == "DQN":
+    model = DQN(policy, E, learning_rate=lr, verbose=1, policy_kwargs=policy_kwargs)
+model.set_logger(new_logger)
+
+while True:
+    model.learn(iteration_training_steps)
